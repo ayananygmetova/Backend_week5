3804f06eff8bb6f7365b6ae8d9751f1b901fa073
ayananygmetova <ayananygmetova@gmail.com>
2020-10-03 14:58:12 +0600

    added receipt

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -9,10 +9,24 @@ import scala.util.matching.Regex
 
 object Main extends App{
 
-  case class Product(fullName:String, name:String, amount:Int, price:Double, totalPrice: Double, number: Option[Int],
+  case class Receipt(namePharmacy: String, BIN: String, VAT: Int, number: String, cashBox: String,
+                     change: Int,orderNum: Int, cheque: String, cashier: String, products: Array[Product])
+
+  case class Product(id: Int,fullName:String, name:String, amount:Int, price:Double, totalPrice: Double, number: Option[Int],
                      volume: Option[String], weight:Option[String], productType: Option[String])
 
-  val index: Regex = "[0-9]\\.$".r
+  val namePharmacy: Regex = ".*ТОО.*".r
+  val BIN: Regex = "БИН\\s*\\d*".r
+  val VAT: Regex = "НДС Серия.*".r
+  val numberR: Regex = " №\\s\\d*".r
+  val cashBox: Regex = "Касса\\d*\\-?\\d*".r
+  val change: Regex = "Смена\\s?\\d*".r
+  val orderNum: Regex = "Порядковый номер чека.*".r
+  val cheque: Regex = "Чек.*".r
+  val cashier: Regex = "Кассир.*".r
+
+
+  val id: Regex = "[0-9]\\.$".r
   val name: Regex = "((\\[.*?])*[a-zA-Z _.!()+=`\"@$#*-]*[a-z\\u0400-\\u04FF]+[a-zA-Z _.!()+=`\"@$#*-]*)+(\\d(\\,\\d)?\\%)?(\\d\\в\\d[a-z\\u0400-\\u04FF]*)?".r
   val amountAndPrice = """(\d+\,0{3}\b) x (\d*\s?\d*\,?0{2}$)""".r("amount","price")
   val totalPrice: Regex = """(^\d*\s?\d*\,?0{2}$)""".r
@@ -27,6 +41,17 @@ object Main extends App{
   var ok = false
   var arr = ArrayBuffer[Product]()
 
+  var nameOfPharmacy = ""
+  var BINOfPharmacy = ""
+  var VATOfPharmacy = ""
+  var numberRec = ""
+  var cachboxOfPharmacy = ""
+  var changeOfPharmacy = ""
+  var orderNumRec = ""
+  var chequeRec = ""
+  var cashierOfPharmacy = ""
+
+  var id_num = 0
   var productFullName = ""
   var productName = ""
   var productPrice:Double = 0
@@ -51,7 +76,17 @@ object Main extends App{
   def parsing() {
 
     for (line <- fSource.getLines) {
-      if (ok) {
+      if(findFirstMatchIn(namePharmacy, line)!=null) nameOfPharmacy = line
+      else if(findFirstMatchIn(BIN, line)!=null) BINOfPharmacy = line.replaceAll("[a-z\\u0400-\\u04FF\\s]", "")
+      else if(findFirstMatchIn(VAT,line)!=null) VATOfPharmacy = line.replaceAll("[a-z\\u0400-\\u04FF\\s]", "")
+      else if(findFirstMatchIn(numberR,line)!=null) numberRec= line.replaceAll(" № ", "")
+      else if(findFirstMatchIn(cashBox,line)!=null) cachboxOfPharmacy =line.replaceAll("[a-z\\u0400-\\u04FF\\s]", "")
+      else if(findFirstMatchIn(change,line)!=null) changeOfPharmacy = line.replaceAll("[a-z\\u0400-\\u04FF\\s]", "")
+      else if(findFirstMatchIn(orderNum,line)!=null) orderNumRec = line.replaceAll("[a-z\\u0400-\\u04FF]", "")replaceAll("   №", "")
+      else if(findFirstMatchIn(cheque,line)!=null) chequeRec = line.replaceAll("Чек №", "")
+      else if(findFirstMatchIn(cashier,line)!=null) cashierOfPharmacy = line.replaceAll("Кассир", "")
+
+      else if (ok) {
         ok = false
         productNumber = if (findFirstMatchIn(number, line) != null) Some(findFirstMatchIn(number, line).drop(1).toInt) else None
         productVolume = matching(volume, line)
@@ -60,12 +95,15 @@ object Main extends App{
         productName = if (findFirstMatchIn(name, line) != null) findFirstMatchIn(name, line) else ""
         productFullName = line
       }
-      if (findFirstMatchIn(index, line) != null) ok = true
-      if (line == "Стоимость") {
-        arr.append(Product(productFullName, productName, productAmount, productPrice, productTotalPrice,
+      else if (findFirstMatchIn(id, line) != null) {
+        ok = true
+        id_num = line.replace(".", "").toInt
+      }
+      else if (line == "Стоимость") {
+        arr.append(Product(id_num,productFullName, productName, productAmount, productPrice, productTotalPrice,
           productNumber, productVolume, productWeight, productType))
       }
-      if (findFirstMatchIn(totalPrice, line) != null)
+      else if (findFirstMatchIn(totalPrice, line) != null)
         productTotalPrice = line.dropRight(3).replace(" ", "").toDouble
 
       val result = amountAndPrice.findFirstMatchIn(line)
@@ -78,13 +116,13 @@ object Main extends App{
       }
     }
     fSource.close()
+
     val file_Object = new File("json.txt" )
     val print_Writer = new PrintWriter(file_Object)
-    for (i<-0 until arr.length) {
-      val json = arr(i).asJson.spaces2
-      print_Writer.write(json)
-      if (i!=arr.length-1) print_Writer.write(",")
-    }
+    val receipt = new Receipt(nameOfPharmacy, BINOfPharmacy,VATOfPharmacy.toInt,  numberRec, cachboxOfPharmacy,
+      changeOfPharmacy.toInt, orderNumRec.toInt, chequeRec, cashierOfPharmacy,  arr.toArray)
+    val json = receipt.asJson.spaces2
+    print_Writer.write(json)
     print_Writer.close()
   }
   parsing()
